c4ba2f6839ec1160a5e703f2ff2c0997ccf9c753
Atharv_Telang <168339563+Atharv-Telang1604@users.noreply.github.com>
2025-09-29 21:11:38 +0530

    Update Login.jsx

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = https
+            const url = `https://deploy-auth-app1-api.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
